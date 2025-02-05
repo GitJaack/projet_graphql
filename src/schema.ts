baa7c46d3eaf0b24c933bdb9ae762d95a88f0b0c
GitJaack <yejack326@gmail.com>
2025-02-05 14:15:49 +0100

    schema

---
+++ src/schema.ts
@@ -0,0 +1,50 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        posts: [Post!]!
+        comments: [Comment!]!
+        likes: [Like!]!
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        content: String!
+        author: User!
+        comments: [Comment!]!
+        likes: [Like!]!
+        createdAt: DateTime!
+    }
+
+    type Comment {
+        id: ID!
+        content: String!
+        author: User!
+        post: Post!
+        createdAt: DateTime!
+    }
+
+    type Like {
+        id: ID!
+        user: User!
+        post: Post!
+    }
+
+    type Query {
+        users: [User!]!
+        user(id: ID!): User
+        posts: [Post!]!
+        post(id: ID!): Post
+    }
+
+    type Mutation {
+        signUp(name: String!, email: String!): User!
+        createPost(title: String!, content: String!): Post!
+        addComment(postId: ID!, content: String!): Comment!
+        likePost(postId: ID!): Like!
+    }
+`;
